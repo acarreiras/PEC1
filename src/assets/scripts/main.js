a2df33a3fda40898953c0e8b1d8a0db315014264
acarreiras <albertocarreiras@gmail.com>
2024-11-02 18:30:14 +0100

    subida a git del proyecto

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,3 +14,38 @@
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+/*Flickity*/
+import Flickity from 'flickity';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const elem = document.querySelector('.carousel');
+  const flickityInstance = new Flickity(elem, {
+    cellAlign: 'center',   
+    contain: true,         
+    wrapAround: false,      
+    autoPlay: 3000,        
+    prevNextButtons: true, 
+    pageDots: false         
+  });
+});
+
+/*Responsive Nav.js*/
+import responsiveNav from 'responsive-nav';
+
+var nav = responsiveNav("#nav",
+  { // Selector
+    animate: true, // Boolean: Use CSS3 transitions, true or false
+    transition: 300, // Integer: Speed of the transition, in milliseconds
+    label: '<i class="fas fa-bars"></i>', // String: Label for the navigation toggle
+  });
+
+  import AOS from 'aos';
+  AOS.init({
+    duration: 100, // Duración de la animación en milisegundos
+    easing: 'ease-in-out', // Curva de velocidad de la animación
+    once: true // La animación se ejecuta solo una vez
+  });
+  console.log("AOS inicializado");
+
+
+
